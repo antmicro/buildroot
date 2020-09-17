94c8e60d0e5e12f87da01d984543a33af848bc37
Grzegorz Latosinski <glatosinski@antmicro.com>
2020-09-17 17:01:28 +0200

    nvidia-driver: Removed restricting using only mesa3d-headers for NVIDIA card
    To provide support for multiple types of GPUs (based on libglvnd), the
    system needs both NVIDIA drivers and mesa3d drivers.
    
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- package/nvidia-driver/nvidia-driver.mk
+++ package/nvidia-driver/nvidia-driver.mk
@@ -20,8 +20,8 @@ ifeq ($(BR2_PACKAGE_NVIDIA_DRIVER_XORG),y)
 # are build dependencies of packages that depend on nvidia-driver, so
 # they should be built prior to those packages, and the only simple
 # way to do so is to make nvidia-driver depend on them.
-NVIDIA_DRIVER_DEPENDENCIES += mesa3d-headers xlib_libX11 xlib_libXext
 NVIDIA_DRIVER_PROVIDES += libgl libegl libgles
+NVIDIA_DRIVER_DEPENDENCIES += mesa3d xlib_libX11 xlib_libXext
 
 # libGL.so.$(NVIDIA_DRIVER_VERSION) is the legacy libGL.so library; it
 # has been replaced with libGL.so.1.0.0. Installing both is technically
@@ -125,7 +125,7 @@ ifeq ($(BR2_PACKAGE_NVIDIA_DRIVER_OPENCL),y)
 NVIDIA_DRIVER_LIBS += \
 	libOpenCL.so.1.0.0 \
 	libnvidia-opencl.so.$(NVIDIA_DRIVER_VERSION)
-NVIDIA_DRIVER_DEPENDENCIES += mesa3d-headers
+NVIDIA_DRIVER_DEPENDENCIES += mesa3d
 NVIDIA_DRIVER_PROVIDES += libopencl
 endif
 
