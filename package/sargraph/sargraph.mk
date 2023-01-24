978a84f05d37a53c5992b843db997e829dce268a
Adam Olech <aolech@antmicro.com>
2023-01-24 13:05:05 +0100

    package: add sargraph

---
+++ package/sargraph/sargraph.mk
@@ -0,0 +1,19 @@
+SARGRAPH_VERSION = 2e0a2ecbfa3a55dba0edbeca21441785e6514826 
+SARGRAPH_SITE = $(call github,antmicro,sargraph,$(SARGRAPH_VERSION))
+SARGRAPH_LICENSE = Apache-2.0
+SARGRAPH_LICENSE_FILES = LICENSE
+SARGRAPH_CUSTOM_INSTALL_DIR = /opt/sargraph
+SARGRAPH_ENTRYPOINT = $(SARGRAPH_CUSTOM_INSTALL_DIR)/sargraph.py
+
+define SARGRAPH_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/$(SARGRAPH_CUSTOM_INSTALL_DIR)
+
+	$(foreach f,$(notdir $(wildcard $(@D)/*.py)),
+		$(INSTALL) -D -m 0644 $(@D)/$(f) \
+			$(TARGET_DIR)/$(SARGRAPH_CUSTOM_INSTALL_DIR)/$(f))
+
+	chmod +x $(TARGET_DIR)/$(SARGRAPH_ENTRYPOINT)
+	ln -sf $(SARGRAPH_ENTRYPOINT) $(TARGET_DIR)/usr/bin/sargraph
+endef
+
+$(eval $(generic-package))
