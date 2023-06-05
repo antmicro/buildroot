390059b9b336062b2f8d959adeb3ed13f3b45ab0
Grzegorz Placzek <gplaczek@antmicro.com>
2023-06-05 15:46:34 +0200

    Add bash-static package

---
+++ package/bash-static/bash-static.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# bash-static
+#
+################################################################################
+
+BASH_STATIC_VERSION = 87e8f814d988486ec1d164d2dafa3ea925fc0146
+BASH_STATIC_LICENSE = GPL-3.0+
+BASH_STATIC_SITE = $(call github,robxu9,bash-static,$(BASH_STATIC_VERSION))
+BASH_STATIC_LICENSE_FILES = COPYING
+
+define BASH_STATIC_BUILD_CMDS
+	cd $(@D) && ./build.sh linux $(BR2_ARCH)
+endef
+
+define BASH_STATIC_INSTALL_TARGET_CMDS
+	cd $(@D) && cp releases/bash $(TARGET_DIR)/opt/bash-static
+endef
+
+$(eval $(generic-package))
